3bd2f5b8ec540dfcdc7e7ec2c62f98e58136a304
wanglei <2934490605@qq.com>
2023-02-02 11:37:35 +0800

    feat: 修改pathList

--- vue-router/createMatcher.js
+++ vue-router/createMatcher.js
@@ -2,12 +2,13 @@ import  createRouteMap  from './create-route-map'
 export default function (routes) {
   //pathList 会把所有路由组成一个数组 ['/','/about','/about/a','/about/b']
   //pathMap { /:{}, /about/a: {}}
-  let  {pathLsit, pathMap} = createRouteMap(routes)
+  let { pathList, pathMap } = createRouteMap(routes)
+  console.log('pathLsit-->', pathList)
   function match() { // 通过用户输入的路径 获取对应的匹配记录
     
    }
   function addRoutes(routes) { // 用户动态添加的路由
-    createRouteMap(routes, pathLsit, pathMap)
+    createRouteMap(routes, pathList, pathMap)
   }
   return {
     match,
